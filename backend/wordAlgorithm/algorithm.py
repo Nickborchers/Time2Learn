c8875f422503b3b14e2cbde3466e65db0db562e2
VictorPreda <v.m.preda@student.rug.nl>
2017-03-12 23:59:06 +0100

    Populated letter Table from provided xml file populated Words array from created xml file 90% word algorithm implemented

--- backend/wordAlgorithm/algorithm.py
+++ backend/wordAlgorithm/algorithm.py
@@ -1,5 +1,9 @@
 import word
 import wordValueTable
+try:
+    import Queue as Q  # ver. < 3.0
+except ImportError:
+    import queue as Q
 
 # make array of words
 
@@ -8,17 +12,33 @@ import wordValueTable
 
 #wordValue = frequencyOfLetterinLanguage + wordlenght + correlation
 #correlation = common letters + letters in same place - missplaced letters 
-#print letterTable
-#w = word.makeWord("trololo","","","","","",0)
-#print(w.originalWord);
-language = "Dutch" #language can be taken from word object xml
+
+
+def correlation(originalWord,translation):
+    n = 0
+    u = zip(originalWord,translation)
+    for i,j in u:
+        if i!=j:
+            n+=1
+    return n
+        
+q = Q.PriorityQueue()
+words = word.words
 letterTable = wordValueTable.letterTable
 letterValues = []
-print letterTable[1].index(language)
-valuesPosition = 2+letterTable[1].index(language);
-letterValues = letterTable[valuesPosition]
-print letterValues
 
-#word length
-#value of the word
-# char originalWord, char translatedWord, char category, int wordValue,char[] extensions,int progress,int wordDifficulty, char[] language
+for word in words:
+    language = word.language
+    valuesPosition = 2+letterTable[1].index(language);
+    letterValues = letterTable[valuesPosition]
+    word.wordValue = len(word.originalWord)
+    for c in word.originalWord:
+        word.wordValue += float(letterValues[ord(c)-ord("a")])
+    word.wordValue = correlation(word.originalWord,word.translatedWord)  #minimal correltion will be further developed
+    
+
+
+for word in words:
+    q.put((word.wordValue,word))
+    
+print q[0].word
--- backend/wordAlgorithm/word.py
+++ backend/wordAlgorithm/word.py
@@ -1,5 +1,20 @@
+# Format of xml taken as input:
+#    <Words>
+#        <Word>
+#            <Language>Dutch</Language>
+#            <Word>ja</Word>
+#            <Translation>yes</Translation>
+#            <Category/>
+#            <Word_Difficulty/>
+#            <Extensions/>
+#        </Word>
+#    </Words>
+
+
+words = []
+
 class Word(object):
-    originalWord="esgbtntyht";
+    originalWord="";
     language = ""
     translatedWord="";
     category="";
@@ -8,14 +23,53 @@ class Word(object):
     progress = 0;
     wordDifficulty = 0;
     
-def makeWord(originalWord,translatedWord,category,wordValue,extensions,progress,wordDifficulty,language):
+def makeWord(language,originalWord,translatedWord,category,extensions,wordDifficulty):
     word = Word()
     word.originalWord = originalWord
     word.translatedWord = translatedWord
     word.category = category
-    word.wordValue = wordValue
     word.extensions = extensions
-    word.progress = progress
     word.wordDifficulty = wordDifficulty
     word.language = language
     return word
+
+
+language = ""
+originalWord = ""
+translatedWord = ""
+category = 0
+extensions = ""
+wordDifficulty = ""
+import xml.etree.ElementTree as ET
+tree = ET.parse('words.xml');
+root = tree.getroot()
+for child in root:
+    for child1 in child:
+        if child1.tag == "Language":
+            language = child1.text
+            
+        if child1.tag == "Word":
+            originalWord = child1.text
+            
+        if child1.tag == "Translation":
+            translatedWord = child1.text
+            
+        if child1.tag == "Category":
+            category = child1.text
+            
+        if child1.tag == "Word_Difficulty":
+            extensions = child1.text
+            
+        if child1.tag == "Extensions":
+            wordDifficulty = child1.text
+            
+        words.append(makeWord(language,originalWord,translatedWord,category,extensions,wordDifficulty))
+        
+
+        
+        
+        
+        
+        
+        
+        
